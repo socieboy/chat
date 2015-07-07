68d75b59e8d31477ef5ccd32a95999e7f42a6e65
Francisco Sepulveda <socieboy@gmail.com>
2015-07-07 16:19:11 -0600

    Fix bug on service provider

--- src/ChatServiceProvider.php
+++ src/ChatServiceProvider.php
@@ -45,14 +45,14 @@ class ChatServiceProvider extends ServiceProvider
         $this->publishes([
 
             // Publish config file
-            __DIR__.'/../Config/chat.php' => base_path('config/chat.php'),
+            __DIR__.'/Config/chat.php' => base_path('config/chat.php'),
 
             // Publish css style
-            __DIR__.'/../Scripts/css/chat.css' => base_path('public/css/chat.css'),
+            __DIR__.'/Scripts/css/chat.css' => base_path('public/css/chat.css'),
 
             // Publish js script
-            __DIR__.'/../Scripts/js/chat.js' => base_path('public/js/chat.js'),
-            __DIR__.'/../Scripts/js/socket.io.js' => base_path('public/js/socket.io.js'),
+            __DIR__.'/Scripts/js/chat.js' => base_path('public/js/chat.js'),
+            __DIR__.'/Scripts/js/socket.io.js' => base_path('public/js/socket.io.js'),
 
         ]);
     }
