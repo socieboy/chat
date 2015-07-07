7e4f99a7d61ad0bbc4194a5567814c63f23b362e
Francisco Sepulveda <socieboy@gmail.com>
2015-07-07 16:03:12 -0600

    Added the scrips and published command

--- src/ChatServiceProvider.php
+++ src/ChatServiceProvider.php
@@ -14,6 +14,8 @@ class ChatServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->publishFiles();
+
         $this->loadViewsFrom(__DIR__.'/Views', 'Chat');
     }
 
@@ -36,5 +38,22 @@ class ChatServiceProvider extends ServiceProvider
         }
 
 
+    }
+
+    protected function publishFiles()
+    {
+        $this->publishes([
+
+            // Publish config file
+            __DIR__.'/../Config/chat.php' => base_path('config/chat.php'),
+
+            // Publish css style
+            __DIR__.'/../Scripts/css/chat.css' => base_path('public/css/chat.css'),
+
+            // Publish js script
+            __DIR__.'/../Scripts/js/chat.js' => base_path('public/js/chat.js'),
+            __DIR__.'/../Scripts/js/socket.io.js' => base_path('public/js/socket.io.js'),
+
+        ]);
     }
 }
